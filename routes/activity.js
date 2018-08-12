515c54a26318ed005c6a3ce95bb9bf704fb7b7b5
npandya-ghub <42293221+npandya-ghub@users.noreply.github.com>
2018-08-12 13:39:37 +0100

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -86,7 +86,7 @@ exports.execute = function (req, res) {
             
             // decoded in arguments
             var decodedArgs = decoded.inArguments[0];
-            
+            console.log(decodedArgs);
             logData(req);
             res.send(200, 'Execute');
         } else {
